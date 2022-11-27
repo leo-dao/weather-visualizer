1a38aee85a9604c04e89f80b4c0016ff68112882
Leo Dao <leotomdao@gmail.com>
2022-11-27 11:04:13 -0500

    Added platform and camera controls

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import { Canvas } from '@react-three/fiber';
-import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+import Search from './Components/Search';
+import Platform from './Components/Platform';
+import Cloud from './Components/Cloud';
+import { OrbitControls } from 'three-stdlib/controls/OrbitControls'
+import { useEffect } from 'react';
+import { useThree } from '@react-three/fiber';
 import './App.css';
+
 
 function App() {
 
-  let data = require('./worldcities.json');
+  const CameraController = () => {
+    const { camera, gl } = useThree();
+    useEffect(() => {
+      const controls = new OrbitControls(camera, gl.domElement);
+
+      controls.minDistance = 3;
+      controls.maxDistance = 20;
+      return () => {
+        controls.dispose();
+      };
+    }, [camera, gl]);
+    return null;
+  };
 
   return (
     <div className="App">
-      <Canvas id='Canvas'>
-
+      <Search />
+      <Canvas id='canvas'>
+        <CameraController />
+        <spotLight position={[0, 100, 0]} angle={Math.PI} penumbra={0} />
+        <Cloud />
+        <Platform />
       </Canvas>
-      <ReactSearchAutocomplete
-        id="search"
-        items={data}
-      />
+
+
     </div>
+
+
   );
 }
 
