655b65bde81aacf0d1a73edefad2cdda12c8b737
Leo Dao <leotomdao@gmail.com>
2022-12-01 16:54:37 -0500

    Made top (search) background transparent, updated style

--- src/Components/search.jsx
+++ src/Components/search.jsx
@@ -8,27 +8,40 @@ const Search = () => {
         latitude: 0
     })
 
+    const [opacity, setOpacity] = React.useState(0)
+
     let data = require('../utils/worldcities.json');
 
     const API_KEY = process.env.REACT_APP_API_KEY
 
     const handleOnSelect = (item) => {
-
         setLocation({
             longitude: item.lng,
             latitude: item.lat
         })
     }
 
+    const onClick = () => {
+        console.log(location)
+        setOpacity(0)
+    }
+
     return (
-        <ReactSearchAutocomplete
-            items={data}
-            onSelect={handleOnSelect}
-            autoFocus
-            styling={{
-                zIndex: 1,
-            }}
-        />
+        <div id='search-container'>
+            <ReactSearchAutocomplete
+                items={data}
+                onSelect={handleOnSelect}
+                onFocus={() => setOpacity(1)}
+                placeholder='Search for a city'
+                styling={{
+                    zIndex: 1,
+                    backgroundColor: `rgba(255, 255, 255, ${opacity})`,
+                }}
+            />
+            <button onClick={onClick}>
+                SEARCH
+            </button>
+        </div>
     );
 }
 
